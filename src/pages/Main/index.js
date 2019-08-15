4a1cace1366aaffaa46492c1d3d860777114d090
marcioper <marcioper@gmail.com>
2019-08-15 16:44:36 -0300

    first commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,13 +2,36 @@ import React from 'react';
 
 import {Text, SafeAreaView, StatusBar} from 'react-native';
 
+import {FloatingAction} from 'react-native-floating-action';
+
 import styles from './styles';
 
 export default function Main() {
+  const actions = [
+    {
+      text: 'Sincronizar',
+      icon: require('~/assets/images/icons/refresh.png'),
+      name: 'bt_refresh',
+      position: 2,
+    },
+    {
+      text: 'Adicionar',
+      icon: require('~/assets/images/icons/edit.png'),
+      name: 'bt_edit',
+      position: 1,
+    },
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="default" />
       <Text style={styles.welcome}>MAPBOX here</Text>
+      <FloatingAction
+        actions={actions}
+        onPressItem={name => {
+          console.tron.log(`selected button: ${name}`);
+        }}
+      />
     </SafeAreaView>
   );
 }
